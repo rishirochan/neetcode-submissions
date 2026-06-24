55b38ca4a923368a50d3e42213d58dc842488f01
Rishi Potipireddi <49170057+rishirochan@users.noreply.github.com>
2026-06-23 21:20:45 -0700

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,23 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+
+        intervals.sort(key=lambda x : x.start)
+        heap = []
+
+        for interval in intervals:
+            if heap and heap[0] <= interval.start:
+                heapq.heapreplace(heap, interval.end)
+            else:
+                heapq.heappush(heap, interval.end)
+        
+        return len(heap)
